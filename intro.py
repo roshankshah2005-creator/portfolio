d8c9bc55a6c9080ce73f763e71c29e50eef47e19
roshankshah2005-creator <roshank.shah2005@gmail.com>
2026-06-11 13:15:37 +0530

    fix image path

--- intro.py
+++ intro.py
@@ -9,7 +9,7 @@ def get_base64(file):
         data = f.read()
     return base64.b64encode(data).decode()
 
-img_base64 = get_base64("rbg.jpg")
+img_base64 = get_base64("images/rbg.jpg")
 
 # ---------- HERO SECTION ----------
 st.markdown(f"""
