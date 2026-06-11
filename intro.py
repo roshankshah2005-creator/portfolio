985859361ce4c2307b334fb33fe9a33df32ef5da
roshankshah2005-creator <roshank.shah2005@gmail.com>
2026-06-11 13:18:40 +0530

    fix image path

--- intro.py
+++ intro.py
@@ -46,7 +46,7 @@ st.write("\n")
 col1, col2 = st.columns([1, 2])
 
 with col1:
-    st.image("photoopo.jpg", width=200)
+    st.image("images/photoopo.jpg", width=200)
 
 with col2:
     st.header("About Me")
