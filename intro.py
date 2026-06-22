f62d9aa5556f32b9e54f18a960d1d11902bcb3c4
roshankshah2005-creator <roshank.shah2005@gmail.com>
2026-06-22 21:01:19 +0530

    Update intro.py

--- intro.py
+++ intro.py
@@ -12,7 +12,7 @@ def get_base64(file):
 
 # Using os.path to make sure Streamlit finds them in the portfolio subfolder
 current_dir = os.path.dirname(os.path.abspath(__file__))
-image_path = os.path.join(current_dir, "rbg.jpg")
+image_path = os.path.join(current_dir, "oip.jpg")  # <-- Updated to your new filename
 
 img_base64 = get_base64(image_path)
 
@@ -31,7 +31,7 @@ st.markdown(f"""
 .hero h1 {{
     font-size: 50px;
     margin-bottom: 10px;
-    color: #f5f5f5; /* Fixed the double ## typo here */
+    color: #f5f5f5;
 }}
 .hero h3 {{
     font-size: 22px;
@@ -51,7 +51,7 @@ st.write("\n")
 col1, col2 = st.columns([1, 2])
 
 with col1:
-    # Changed file name here from photoopo.jpg to image.jpg
+    # Looks for your profile photo named 'image.jpg'
     photo_path = os.path.join(current_dir, "image.jpg")
     st.image(photo_path, width=200)
 
